2bf8a7d2c1fe7375cdb901cb2c08400b4352874c
Yurii Kukharuk <yurakukharuk@gmail.com>
2022-10-16 22:30:23 +0300

    Finish task

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,29 @@
+import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+import css from '../ImageGallery/ImageGallery.module.css';
+import { PropTypes } from 'prop-types';
+
+
+const ImageGallery = ({ items, onClick }) => {
+  console.log(items);
+
+  const elements = items.map(({ webformatURL, id, largeImageURL, tags }) =>
+    <ImageGalleryItem
+      webformatURL={webformatURL}
+      onClick={onClick}
+      largeImageURL={largeImageURL}
+      key={id}
+      tags={tags}
+    />);
+  return (
+    <ul className={css.imageGallery}>
+      {elements}
+    </ul>
+  );
+};
+
+export default ImageGallery;
+
+ImageGallery.propTypes = {
+  items: PropTypes.PropTypes.array.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
