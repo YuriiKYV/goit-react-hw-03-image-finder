e09b6f38a5ceb6ff783333db60696534207a5191
Yurii Kukharuk <yurakukharuk@gmail.com>
2022-10-16 22:35:21 +0300

    Delete console.loge

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -4,7 +4,6 @@ import { PropTypes } from 'prop-types';
 
 
 const ImageGallery = ({ items, onClick }) => {
-  console.log(items);
 
   const elements = items.map(({ webformatURL, id, largeImageURL, tags }) =>
     <ImageGalleryItem
